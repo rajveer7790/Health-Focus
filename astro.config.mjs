fc92c955763386da200bcc303fd2654bf95030ae
rajveer7790 <rajchaudhary.nie@gmail.com>
2026-02-28 17:51:08 +0530

    fix: Generate single sitemap.xml to resolve GSC issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
-import sitemap from '@astrojs/sitemap';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://healthfocus.fit',
-  integrations: [mdx(), sitemap({
-    filter: (page) => page !== 'https://healthfocus.fit/404/',
-  }), tailwind()],
+  integrations: [mdx(), tailwind()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
